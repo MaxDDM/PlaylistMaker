0f1c9735bd3ab5d33413f1ee144a064217461c80
MaxDDM <gma270304@gmail.com>
2026-07-20 16:25:54 +0300

    complete 12th sprint HW

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.retrofit2:retrofit:2.12.0")
     implementation("com.squareup.retrofit2:converter-gson:2.12.0")
+    implementation("com.google.code.gson:gson:2.11.0")
 }
--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -2,4 +2,4 @@ package com.example.playlistmaker
 
 import com.google.gson.annotations.SerializedName
 
-class Track(val trackName: String, val artistName: String?, @SerializedName("trackTimeMillis") var trackTime: Long?, val artworkUrl100: String) { }
+class Track(val trackId : String, val trackName: String, val artistName: String?, @SerializedName("trackTimeMillis") var trackTime: Long?, val artworkUrl100: String) { }
